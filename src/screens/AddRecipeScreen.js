9073250ba9821aa4beb1685156b1ac55dd499712
Paola Lisboa <joselinlisboa21@gmail.com>
2026-03-24 21:04:00 -0500

    First Commit

---
+++ src/screens/AddRecipeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Text, TextInput, View, TouchableOpacity} from 'react-native'
+
+const AddRecipeScreen = () =>{
+    
+    return(
+        <View>
+            <Text>Hola mundo</Text>;
+        </View>
+    )
+}
+
+export default AddRecipeScreen;
